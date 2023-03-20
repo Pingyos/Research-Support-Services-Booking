83e1df8e29920bd4a4ce0a93760a2781e659cb23
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-03-20 10:10:01 +0700

    update

---
+++ booking.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once 'head.php'; ?>
+<body>
+    <?php require_once 'header.php'; ?>
+    <main id="main">
+
+    </main>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <?php require_once 'script.php'; ?>
+</body>
+</html>
