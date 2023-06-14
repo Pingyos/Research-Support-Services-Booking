364a6d5116849dcdfda4f56968a27f87352fec4c
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-06-14 09:56:16 +0700

    update

--- bookingtime.php
+++ bookingtime.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-session_start();
-if (!isset($_SESSION['login_info'])) {
-    header('Location: login.php');
-    exit;
-}
-if (isset($_SESSION['login_info'])) {
-    $json = $_SESSION['login_info'];
-} else {
-    echo "You are not logged in.";
-}
+// session_start();
+// if (!isset($_SESSION['login_info'])) {
+//     header('Location: login.php');
+//     exit;
+// }
+// if (isset($_SESSION['login_info'])) {
+//     $json = $_SESSION['login_info'];
+// } else {
+//     echo "You are not logged in.";
+// }
 require_once 'head.php';
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
